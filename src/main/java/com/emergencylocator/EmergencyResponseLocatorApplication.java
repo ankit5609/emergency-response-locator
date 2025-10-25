9046a4f0054fce69342cea2bee9e34d60b91da4e
ankit5609 <86094718+ankit5609@users.noreply.github.com>
2025-10-25 23:06:44 +0530

    EmergencyResponseLocatorApplication.java

---
+++ src/main/java/com/emergencylocator/EmergencyResponseLocatorApplication.java
@@ -0,0 +1,5 @@
+public class EmergencyResponseLocatorApplication {
+    public static void main(String[] args) {
+        System.out.println("Emergency Response Locator Backend Prototype");
+    }
+}
